4482fe18d9597fc5a5a1d09f823651489d5cd362
= <joshuaraftery99@gmail.com>
2024-07-18 11:03:50 +0100

    Task 18 completed! 200 Tests for response body content, ignoring additonaly properties in request, handling negetive numbers, 400 and 404 tests for parametric endpoint, bad type in request and missing inc_votes property in request. Updated endpoints.json

--- routers/comments-router.js
+++ routers/comments-router.js
@@ -1,6 +1,7 @@
-const { removeComment } = require("../controllers/commentsControllers")
+const { removeComment, patchComment } = require("../controllers/commentsControllers")
 const commentsRouter = require("express").Router()
 
 commentsRouter.delete('/:comment_id',removeComment)
+commentsRouter.patch('/:comment_id',patchComment)
 
 module.exports = commentsRouter
